dcdc0bf29030e9aff7369031dc890a055bfb0aef
DavidOdhiambo1 <davidworkmail2021@gmail.com>
2024-10-11 08:29:25 +0300

    dom manipulation

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+const main = document.querySelector('main')
+main.remove()
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = "David Odhiambo is the champion"
+
+
+
+
+
+
+
+
